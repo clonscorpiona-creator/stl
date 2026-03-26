08ca4c9aad99fde68983a281cf76e42df089406a
Ваше Имя <you@example.com>
2026-03-26 21:51:18 +0300

    Добавлена кнопка Чат в панель навигации

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3n72tqhtim!zx2a+8swiu@cv%95kjljb=#98zor^eowlf+w^(d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'testserver']
 
 
 # Application definition
